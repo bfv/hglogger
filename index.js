63bcbb1cffcfd7d73df469ba2b57e1ef34be5eb8
bfv <dev@bfv.io>
2018-03-25 11:46:47 +0200

    documentation; streamlining code

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var i2c = require('i2c-bus');
 var i2c1 = i2c.openSync(1);
 
+// device i2c address
 const MPL3115A2_ADDR = 0x60;
+
+// control register 1;
 const CTRL_REG1 = 0x26;
+
+// PT data configuration register address
 const PT_DATA_CFG = 0x13;
-const DREM = 0x04;
-const PDEFE = 0x02;
-const TDEFE = 0x01;
+const DREM = 0x04;   // raised data events
+const PDEFE = 0x02;  // add pressure events
+const TDEFE = 0x01;  // add temperature events
 
 function toCelsius(rawTemp) {
   var halfDegrees = ((rawTemp & 0xff) << 1) + (rawTemp >> 15);
@@ -21,21 +26,22 @@ function toCelsius(rawTemp) {
 function calcPressure(msb, csb, lsb) {
   var pressure;
 
-  pressure = msb << 8;
-  pressure += csb;
-  pressure = pressure << 2;
-  pressure += (lsb & 0xc0) >> 6;
+  // use msb, csb and bit 7&6 of lsb for integer part  (of Q18.2 fixed point)
+  pressure = (((msb << 8) + csb) << 2) + ((lsb & 0b11000000) >> 6);
+
+  // add fractional part, 2 bit => resolution = 0.25 Pa
   pressure += ((lsb & 0b00100000) >> 5) / 2;
   pressure += ((lsb & 0b00010000) >> 4) / 4;
 
-  return pressure / 100;
-
+  return pressure / 100;  // return hPa (millibar)
 }
 
 (function() {
-  i2c1.writeByteSync(MPL3115A2_ADDR, CTRL_REG1, 0x38);
+  // standby, oversample ratio (OSR) 128 => min. sample time 512ms
+  i2c1.writeByteSync(MPL3115A2_ADDR, CTRL_REG1, 0b00111000);
   i2c1.writeByteSync(MPL3115A2_ADDR, 0x13, DREM | PDEFE | TDEFE);
-  i2c1.writeByteSync(MPL3115A2_ADDR, CTRL_REG1, 0x39);
+  // set active, OSR 128
+  i2c1.writeByteSync(MPL3115A2_ADDR, CTRL_REG1, 0b00111001);
 
   while((i2c1.readByteSync(MPL3115A2_ADDR, 0x00) & 0x08) === 0){}
 
